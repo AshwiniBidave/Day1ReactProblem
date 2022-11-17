b2bebb47bd42682936f9e3c9512c137c0de47bfc
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-11-17 16:52:31 +0530

    ADD:added uc1 sucessfully

---
+++ assignment1rct/src/Uc1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+class Uc1 extends React.Component {
+    render() {
+        return (<>
+            <h1>Hello from Bridgelabz</h1>
+        </>
+        );
+    }
+}
+export default Uc1;
