7cf9ae9114bf299712d63167564c06073327cda6
AshwiniBidave <109209046+AshwiniBidave@users.noreply.github.com>
2022-11-23 17:00:45 +0530

    ADD:Added uc8

--- assignment1rct/src/Uc8.js
+++ assignment1rct/src/Uc8.js
@@ -2,41 +2,31 @@ import React from "react";
 import "./App.css";
 function Uc8() {
 	return (
+               <body>
+		   <header className="header">Header</header>
+		   <div className="maincontainer">
+				<sidebar className="sidebar-left">Left</sidebar>
+				<section className="main">
+					<article><h2> First Article</h2>
+						<p>This is a test. This is a test. This is a test. This is a test.
+						   This is a test. This is a test. This is a test. This is a test. This is a test.
+						</p>
+					</article>
+					<article><h2> Second Article</h2>
+						<p>This is a test. This is a test. This is a test. This is a test.
+						   This is a test. This is a test. This is a test. This is a test. This is a test.
+						</p>
+					</article>
+					<article><h2> Third Article</h2>
+						<p>This is a test. This is a test. This is a test. This is a test.
+						   This is a test. This is a test. This is a test. This is a test. This is a test.
+						</p>
+					</article>
+				</section>
 
-		<body>
-			<div id="header">Header</div>
-			<div id="sidebar-left">Left</div>
-			<div id="main">
-				<div id="First"><strong>First Article</strong>
-					<br/>This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-                </div>				
-				<div id="two"><strong>Second Article</strong>
-					<br/>This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-                </div>
-				<div id="three"><strong> Third Article</strong>
-					<br/>This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-                </div>
-				<div id="four"><strong> Four Article</strong>
-					<br/>This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					This is a test. This is a test. This is a test. This is a test. This is a test.
-					<br/>This is a test. This is a test. This is a test. This is a test. This is a test.
- 
-                </div>
+				<sidebar className="sidebar-right">Right</sidebar>
 			</div>
-			<div id="sidebar-right">Right</div>
-			<div id="footer">Footer</div>
+			<footer className="footer">Footer</footer>
 		</body>
 
 	);
